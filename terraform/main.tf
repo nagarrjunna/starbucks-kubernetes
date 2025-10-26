fbd014bc4fb0719d8d39d7b535d1e66125824897
nagarrjunna <93728219+nagarrjunna@users.noreply.github.com>
2025-10-26 09:26:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "ec2_security_group" {
 }
 
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
+ami = "ami-00af95fa354fdb788"  
 instance_type = "t2.medium"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
